492c03b007ce5500f8a903a18f84bbb480a65b43
Reginah24 <r.majura@alustudent.com>
2026-05-17 21:39:56 +0200

    line

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -12,4 +12,4 @@ def cat_matrices2D(mat1, mat2, axis=0):
         if len(mat1) != len(mat2):
             return None
         return [mat1[i] + mat2[i] for i in range(len(mat1))]
-    
+    return None
